567c8cb6bf3921fbb502675f16c31bbd38a4347e
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-10 06:02:42 -0800

    alx-low_level_programming hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+		dprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+		return (0);
+}
