26d0ffa4b48fe89273b396fc7d06b5aa337f3960
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-10 06:05:44 -0800

    alx-low_level_programming hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-		dprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+		dprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 		return (0);
 }
