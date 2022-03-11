8273cd11f642f6a50feec60385c611fe9630f36f
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-11 02:01:09 -0800

    alx-variables and if else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	else if (y == 0)
 		printf("Last digit of %d is %d and is 0\n", n, y);
 	else if (y < 6 && y != 0)
-		printf("Last digit of %d is %d and is lesstan 6 and not 0\n", n, y);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
 	return (0);
 }
 
