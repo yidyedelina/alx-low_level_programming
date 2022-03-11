9f206c0a7103c3bbb7e6303740130d171c8aa21f
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-11 01:58:42 -0800

    alx-variables and if else

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description - return a last digit number
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int y;
+
+	y = n % 10;
+	if (y > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, y);
+	else if (y == 0)
+		printf("Last digit of %d is %d and is 0\n", n, y);
+	else if (y < 6 && y != 0)
+		printf("Last digit of %d is %d and is lesstan 6 and not 0\n", n, y);
+	return (0);
+}
+
