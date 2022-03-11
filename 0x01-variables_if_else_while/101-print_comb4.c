c5cb18b77740253b1a31e1bae2e3faf36f626a5a
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-11 04:32:01 -0800

    alx-variables and if else

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,11 +7,13 @@
  */
 int main(void)
 {
-	for (int i = 0; i <= 7; i++)
+	int i, j, k;
+	
+	for (i = 0; i <= 7; i++)
 	{
-		for (int j = i + 1; j <= 8; j++)
+		for (j = i + 1; j <= 8; j++)
 		{
-			for (int k = j + 1; k <= 9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
