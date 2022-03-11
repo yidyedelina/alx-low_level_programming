a6755fc3c2d787cf977a4a48accf5bec09a22c3e
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-11 01:59:55 -0800

    alx-variables and if else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,11 +10,10 @@
 int main(void)
 {
 	int n;
+	int y;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int y;
-
 	y = n % 10;
 	if (y > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, y);
