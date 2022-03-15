ca05d02133d041069c42ef410c14d766a31247a9
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-15 03:33:19 -0700

    function_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,9 @@
-#include "_putchar.c"
+#include "main.h"
+/**
+ * main - Entry point
+ * description: to print alphabet letters
+ * Return: Always 0 (sucees)
+ */
 void print_alphabet(void)
 {
 	char a;
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN
 
 int _putchar(char c);
-
+void print_alphabet(void);
 
 
 #endif
