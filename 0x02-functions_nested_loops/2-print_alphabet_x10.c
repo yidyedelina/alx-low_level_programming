6e5301fc1d13974826ee86fc7fd304040bb913ef
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-15 03:42:37 -0700

    function and nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,3 +1,9 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Description - a function prints alphabet 10 times
+ * Return: always 0 (success)
+ */
 void print_alphabet_x10(void)
 {
 	char a;
@@ -5,7 +11,7 @@ void print_alphabet_x10(void)
 
 	a = 'a';
 	i = 1;
-	while(i <= 10)
+	while (i <= 10)
 	{
 		while (a <= 'z')
 		{
