d03e12a13b4f522ba8d795994444db8f35ecab3d
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-15 06:14:32 -0700

    function and nested loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int first;
+	int second;
+	int seq;
+
+	i = 3;
+	first = 1;
+	second = 2;
+	printf("%d, %d, ", first, second);
+	while(i <= 50)
+	{
+		seq = first + second;
+		first = second;
+		second = seq;
+		printf("%d, ", seq);
+	}
+	printf('\n');
+	return (0);
+}
