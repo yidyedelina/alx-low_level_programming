d93a0e01af92a41233f7d4d752ad3868dfb92f48
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-15 05:59:53 -0700

    function and nested loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int sum;
+
+	n = 1024;
+	while (n > 3)
+	{
+		n--;
+		if (n % 3 || n % 5)
+		{
+			sum = sum + n;
+		}
+	}
+	printf("%d\n", sum);
+}
