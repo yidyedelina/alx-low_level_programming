daa85394c7ef497f2ab69468e8505c5f4c131dcf
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-15 05:35:05 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - printing a number starting from n to 98
+ * @n: input integer argument 
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98){
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				_putchar(" ");
+				_putchar(',');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				_putchar(" ");
+				_putchar(',');
+			}
+		}
+	}
+}
