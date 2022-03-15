dc8e2b14b9c4006625496da084a1aa61cab7a20d
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-15 04:13:14 -0700

    function and nested loops

--- 0x02-functions_nested_loops/5-print_sign.c
+++ 0x02-functions_nested_loops/5-print_sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - print a sign of a given number
  * @n: an integer
- * Return - 1 for positive number, 0 for zero
+ * Return:1 if @n is positive number, 0 for zero
  * and -1 for negative number
  */
 int print_sign(int n)
@@ -17,7 +17,8 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else{
+	else
+	{
 		_putchar('-');
 		return (-1);
 	}
