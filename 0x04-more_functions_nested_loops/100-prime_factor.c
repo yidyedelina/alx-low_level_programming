0a55100e18820e587c89789230dd930d7258b9ab
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-17 11:42:25 -0700

    function and nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int lt, count, i, j, num;
+
+	num = 612852475143;
+	lt = 1;
+	count = 0;
+	for (i = 2; i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			for (j = i - 1; j > 1; j--)
+			{
+				count = 0;
+				if (i % j == 0)
+				{
+					count = 1;
+					break;
+				}
+			}
+			if (count == 0)
+			{
+				if (lt < i)
+				{
+					lt = i;
+				}
+			}
+		}
+		count = 0;
+	}
+	printf("%d", lt);
+	return (0);
+}
