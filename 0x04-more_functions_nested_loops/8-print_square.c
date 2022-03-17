29bed0850ac0130ab980f1328ececfda93b9ce7d
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-17 05:15:28 -0700

    function an nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,9 +10,9 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (int i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (int j = 0; j < size; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
