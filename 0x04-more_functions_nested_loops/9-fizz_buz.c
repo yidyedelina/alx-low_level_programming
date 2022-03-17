267cdb5e0555d38bbd6c6d10c62e2a61ff0802d7
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-17 09:29:24 -0700

    function and nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i  % 5 == 0)
+		{
+			printf("FizzBuzz");
+			putchar(' ');
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+			putchar(' ');
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			putchar(' ');
+		}
+		else
+		{
+			printf("%d", i);
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
