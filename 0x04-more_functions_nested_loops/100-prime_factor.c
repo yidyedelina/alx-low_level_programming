6e5cb5d373ef38f587aa77c42fd3e5c3aab37227
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-17 11:50:50 -0700

    functions and nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,38 +1,21 @@
 #include <stdio.h>
 /**
- * main - entry point
- * Return: always 0
- */
+* main - entry block
+* @void: no argument
+* Return: 0
+*/
 int main(void)
 {
-	long int lt, count, i, j, num;
+	long i, number = 612852475143;
 
-	num = 612852475143;
-	lt = 1;
-	count = 0;
-	for (i = 2; i <= num; i++)
+	for (i  = 2; i <= number; i++)
 	{
-		if (num % i == 0)
+		if (number % i == 0)
 		{
-			for (j = i - 1; j > 1; j--)
-			{
-				count = 0;
-				if (i % j == 0)
-				{
-					count = 1;
-					break;
-				}
-			}
-			if (count == 0)
-			{
-				if (lt < i)
-				{
-					lt = i;
-				}
-			}
+			number = number / i;
+			i--;
 		}
-		count = 0;
 	}
-	printf("%ld\n", lt);
+	printf("%lu\n", i);
 	return (0);
 }
