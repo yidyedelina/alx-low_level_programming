07169ab3a0ae47eac82353d5095e021a73bf14ae
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-23 23:48:25 -0700

    some improvement

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "string.h"
+#include <stdbool.h>
 /**
  * cap_string - capitalize every words
  * @s: string
@@ -8,11 +9,13 @@
 char *cap_string(char *s)
 {
 	int len, i;
+	bool exp;
 
 	len = strlen(s);
 	for (i = 0; i < len; i++)
 	{
-		if ((s[i] >= 32 && s[i] <= 47) || s[i] == '\n' || s[i] == '\t')
+		exp = (s[i] == 32) || (s[i] == 44) || (s[i] == 40) || (s[i] == 41) || (s[i] == 46) || (s[i] == 33) || (s[i] == 63) || (s[i] == 59) || (s[i] == 123) || (s[i] == 125);
+		if (exp || s[i] == '\n' || s[i] == '\t')
 		{
 			if (s[i + 1] >= 97 && s[i + 1] <= 122)
 				s[i + 1] = s[i + 1] - 32;
