5c3d3c26ef93c7f9caf0375609b6fe199b1276b4
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-23 22:03:17 -0700

    improvement

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,26 @@ int _strcmp(char *s1, char *s2)
 	int i, len, value = 0;
 
 	len = strlen(s1);
-	for (i = 0; i < len && s2[i] != '\0';  i++)
+	for (i = 0; i < len;  i++)
 	{
 		if (s1[i] == s2[i])
 			continue;
+		else if (s1[i] == '/0')
+		{
+			value = -15;
+			break;
+		}
+		else if (s2[i] == '/0')
+		{
+			value = 15;
+			break;
+		}
 		else if (s1[i] > s2[i])
 		{
 			value = 15;
 			break;
 		}
+
 		else
 		{
 			value = -15;
