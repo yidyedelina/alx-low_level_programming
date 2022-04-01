60a2e074171b686e1c6f5f8901bfa2ee3c2051bd
yidyedelina <yidnekachewtebeje@gmail.com>
2022-03-31 23:10:45 -0700

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* @argc: the number of argument
+* @argv: a pointer to argument strings
+* Return: always 1
+*/
+
+int main(int argc, char *argv[])
+{
+	puts(argv[0]);
+	return (0);
+}
