ed867f211f93cc3020efdf97fc3140e8796fba58
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-01 09:57:50 -0700

    argc and argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
+
 /**
-* main - the entry point
-* @argc: number of argument
-* @argv: a string of argument
-* Return: always 0
-*/
-int main(int argc, char *argv[])
+ * main - prints its name, followed by a new line
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	char *p;
-	p = "char";
-	if(argv[0] == p)
-		printf("hello");
-	printf("%d \n", argc - 1);
+	if (argc > 0)
+		printf("%d\n", argc - 1);
 	return (0);
 }
