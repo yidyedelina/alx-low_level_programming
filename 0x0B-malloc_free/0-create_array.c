13bedb5359c55a28f4808fceef15183290897804
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-04 09:57:42 -0700

    dynammic memory allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	p = (char dy*)malloc(sizeof(char) * size);
+	p = (char *)malloc(sizeof(char) * size);
 	if (p == NULL)
 	{
 		return (NULL);
