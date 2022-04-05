f9567dc738a64f5efa759a6a1fd433661a187dbb
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-05 10:12:02 -0700

    dynamic allocation

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -40,7 +40,7 @@ char **strtow(char *str)
 	words = word_counter(str);
 	if (words < 1)
 		return (NULL);
-	strDup = malloc(sizeof(char *) * (words + 1));
+	strDup = malloc(sizeof(char *) * (words) + 1);
 	if (strDup == NULL)
 		return (NULL);
 	i = 0;
