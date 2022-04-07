c6a7641beaf57c2d93b09d0b861b291fb575daa2
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-07 11:12:54 -0700

    dynamic allocation

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ * _realloc - reallocate the memory
+ * @ptr: the old memory block
+ * @old_size: the size of the old memory place
+ * @new_size: the size of the new memory place
+ * Return: a void pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned char *p;
+	int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	if (ptr == NULL)
+	{
+		return (p);
+	}
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			p[i] = ((char *)ptr)[i];
+		free(ptr);
+		return (p);
+	}
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			p[i] = ((char *)ptr)[i];
+		free(ptr);
+		return (p);
+	}
+}
