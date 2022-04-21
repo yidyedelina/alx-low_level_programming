4029f3e68f58d2e64a6593e536e48cbdc0dd8bc4
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-21 19:52:02 +0300

    updates

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_list - print the linked list element
+ * @h: the head of linked list
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+	const list_t *traverse = h;
+
+	while (traverse != NULL)
+	{
+		traverse = traverse->next;
+		i++;
+	}
+	return (i);
+}
