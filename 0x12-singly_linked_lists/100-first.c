5a9c8a99288d8f1a8e1911b53e8f6d38c5e17b24
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-21 21:12:47 +0300

    updates

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,11 @@
 #include "lists.h"
 #include "stdio.h"
+/**
+ * myStartupFun - this function runs before main
+ * Return: always nothing
+ */
 
-
-void myStartupFun (void) __attribute__ ((constructor));
+void myStartupFun(void) __attribute__ ((constructor));
 void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
