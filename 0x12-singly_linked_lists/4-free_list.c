02ab70759c592ec9a3c1ab92bda822dfcffca020
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-21 20:57:15 +0300

    free_list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <lists.h>
+/**
+ * free_list - a function free list_t
+ * @head: the head of linked list
+ * Return:void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+	
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
