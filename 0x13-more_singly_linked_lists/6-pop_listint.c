ddea0bca577d627e258e468a82af5a971cc9c770
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-25 22:54:16 +0300

    updating the pop function

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,25 +2,26 @@
 #include <stdlib.h>
 /**
  * pop_listint - delete the head node
- * @head
+ * @head: the head node
  * Return: the head node data
  */
 
 int pop_listint(listint_t **head)
 {
-    int n;
-    listint_t *tmp;
-    if (head == NULL)
-        return 0;
-    tmp = *head;
-    if (tmp->next == NULL)
-    {
-        n = tmp->n;
-        free(tmp);
-        return (n);
-    }
-    n = tmp->n;
-    *head = (*head)->next;
-    free(tmp);
-    return (n);
+	int n;
+	listint_t *tmp;
+
+	if (head == NULL)
+		return (0);
+	tmp = *head;
+	if (tmp->next == NULL)
+	{
+		n = tmp->n;
+		free(tmp);
+		return (n);
+	}
+	n = tmp->n;
+	*head = (*head)->next;
+	free(tmp);
+	return (n);
 }
