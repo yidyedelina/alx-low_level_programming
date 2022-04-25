c13e379253342028db225d295c3443b102ccaa66
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-25 22:32:34 +0300

    fixing pointer issue on question 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,12 @@
  * Return:void
  */
 
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 	listint_t *traverse;
 
-	traverse = head->next;
+	traverse = (*head)->next;
 	head = NULL;
 	while (traverse != NULL)
 	{
