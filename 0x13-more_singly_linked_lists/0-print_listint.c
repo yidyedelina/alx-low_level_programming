119b722c9e08f1a84baacedcd69c4c6f6c6c9cc4
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-25 21:38:34 +0300

    solving betty issues for 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * print_list - print the linked list element
+ * print_listint - print the linked list element
  * @h: the head of linked list
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t i = 0;
-    const listint_t *traverse = h;
+	size_t i = 0;
+	const listint_t *traverse = h;
 
-    while (traverse != NULL)
-    {
-        printf("%d\n", traverse->n);
-        i++;
-        traverse = traverse->next;
-    }
-    return (i);
+	while (traverse != NULL)
+	{
+		printf("%d\n", traverse->n);
+		i++;
+		traverse = traverse->next;
+	}
+	return (i);
 }
