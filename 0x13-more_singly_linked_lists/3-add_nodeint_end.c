cc52edc858300369fceeb253b714f43df89cd920
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-25 22:07:26 +0300

    adding third question

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_nodeint_end - add an int at the tail
+ * @head: the starting of linked list
+ * @n: the value append
+ * Return: the list we append
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *traverse;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	traverse = *head;
+	while (traverse->next != NULL)
+	{
+		traverse = traverse->next;
+	}
+	traverse->next = new;
+	return (*head);
+}
