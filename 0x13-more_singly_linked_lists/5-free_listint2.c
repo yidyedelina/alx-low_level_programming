c3a5ef885ccd2b6853449f34e7622f9a9baf8fc5
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-25 22:27:41 +0300

    uploadint the 5th question

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include "stdlib.h"
+/**
+ * free_listint2 - a function free list_t
+ * @head: the head of linked list
+ * Return:void
+ */
+
+void free_listint2(listint_t *head)
+{
+	listint_t *tmp;
+	listint_t *traverse;
+
+	traverse = head->next;
+	head = NULL;
+	while (traverse != NULL)
+	{
+		tmp = traverse;
+		traverse = traverse->next;
+		free(tmp);
+	}
+}
