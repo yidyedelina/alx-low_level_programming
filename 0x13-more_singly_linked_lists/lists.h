3125363a625ec2858ce88f03055159b9157f89be
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-25 21:45:44 +0300

    betty error fixed in list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,18 +1,18 @@
-#if !defined (MAIN_H)
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
-#include <stddef.h>
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 
