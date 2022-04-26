0204d22206760429fe9670bac435621a604fd2fc
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-26 18:16:12 +0300

    uploading the get_nodeint_at_index method

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - a function to access ith element
+ * @head: the head of the linked linst
+ * @index: the index we need to access element
+ * Return: a node at ith position
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *traverse;
+	unsigned int i = 0;
+
+	traverse = head;
+	while (traverse != NULL)
+	{
+		if (i == index)
+		{
+			return (traverse->n);
+		}
+		traverse = traverse->next;
+		i++;
+	}
+	return (NULL);
+}
