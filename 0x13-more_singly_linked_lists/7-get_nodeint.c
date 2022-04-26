e93e9d09423f8c3b09410d6e3f1721e383963af7
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-26 18:18:45 +0300

    fixing error on get_nodeint_at_index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		if (i == index)
 		{
-			return (traverse->n);
+			return (traverse);
 		}
 		traverse = traverse->next;
 		i++;
