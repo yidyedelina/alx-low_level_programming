fb2325443655532c7ca0c940c2c8d5d5efae010d
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-26 19:22:23 +0300

    fixing error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		newnode->next = traverse;
+		*head = newnode;
 		return (newnode);
 	}
 	while (traverse2 != NULL)
