317e03fd6a1978a1d8406d45ce66c9b0fee10299
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-27 00:46:33 +0300

    uploading 13 question

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,6 +14,11 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+typedef struct listint_safe
+{
+	const listint_t *addy;
+	struct listint_safe *next;
+} listint_safe;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -26,4 +31,5 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
 #endif
