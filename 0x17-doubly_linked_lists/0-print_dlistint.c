f24bc62dfdd0b2dca431f02ca678f11cc3584907
yidye <yidye@pop-os.localdomain>
2022-06-09 20:05:04 +0300

    first question

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_dlistint - print doubly linked list
+ * @h: the head of doubly linked listt
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    const dlistint_t *traverse = h;
+    int n = 0;
+    while (traverse != NULL)
+    {
+        printf("%d\n", traverse->n);
+        n++;
+        traverse = traverse->next;
+    }
+    return (n);
+}
