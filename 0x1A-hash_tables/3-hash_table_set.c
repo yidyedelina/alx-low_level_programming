8a460687b81112e621151d1507fc4c6a22bcc2e6
yidye <yidye@pop-os.localdomain>
2022-07-01 20:02:18 +0300

    3rd assignment

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * hash_table_set - add to hash table
+ * @ht: hash table
+ * @key: the key
+ * @value: the value
+ * Return: 1 if succeeded or 0 if it is not
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *c;
+	hash_node_t *node;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (node == NULL)
+	{
+		return (0);
+	}
+	node->key = (char *)key;
+	node->value = (char *)value;
+	c = ht->array[index];
+	if (c != NULL)
+	{
+		node->next = c;
+		ht->array[index] = node;
+		return (1);
+	}
+	ht->array[index] = node;
+	node->next = NULL;
+	return (1);
+}
